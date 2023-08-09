bcee3345c933058967745f2906e51354dcf235e2
Jerry Li <jerryw.li@mail.utoronto.ca>
2023-08-09 15:43:13 -0400

    Removed unused import in RenameController.java

--- app/src/main/java/com/example/notes2text/adapters/RenameController.java
+++ app/src/main/java/com/example/notes2text/adapters/RenameController.java
@@ -3,7 +3,6 @@ package com.example.notes2text.adapters;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
