dea2d1b1dbbeeef8ef3fdbd86ea3ce5a631f0231
ChowdhuryShaswata <shaswata.03@gmail.com>
2023-08-12 22:41:19 -0400

    Implemented a highest possible accessible layer so that the back button does not send the user beyond safe to access file levels.

--- app/src/main/java/com/example/notes2text/adapters/directoryadapters/fragments/DirectoryAccessController.java
+++ app/src/main/java/com/example/notes2text/adapters/directoryadapters/fragments/DirectoryAccessController.java
@@ -2,6 +2,7 @@ package com.example.notes2text.adapters.directoryadapters.fragments;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -168,6 +169,7 @@ public class DirectoryAccessController extends Fragment {
                             Fragment fragment = DirectoryAccessController.newInstance(higherPath);
                             ((ActivitySwitchController) requireActivity()).replaceFragment(fragment);
                             directoryPresenter.BackLayerSuccess(getActivity());
+                            Log.i("directory", "Went back a layer on the directory controller");
                         } else{
                             Toast.makeText(getActivity(), "Highest layer reached", Toast.LENGTH_SHORT).show();
                         }
